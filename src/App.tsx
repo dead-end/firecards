4a1405ef4570e8cb0bcc736d0dc6388f04e9c69e
Dead End <dead.end@inbox.ru>
2022-11-25 22:28:39 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
+import NotFound from "./routes/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h2>Firecards</h2>
+    <div className="App container">
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">
+            Firecards
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/not-found">Not Found</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
